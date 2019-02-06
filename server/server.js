d5fcac6b9b639c891016853e9fb2c8e26dc169c6
Willem van Duijn <willem.vanduijn@kpn.com>
2019-02-06 09:32:24 +0100

    Added message form & jquery

--- server/server.js
+++ server/server.js
@@ -16,30 +16,29 @@ app.use(express.static(clientPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
-    // socket.emit from Admin > Welcome to chat app
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
-
-    // socket.broadcast.emit > new user joined for all other users but yourself
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
-
-    socket.on('createMessage', (message) => {
-        console.log('createMessage');
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date.getTime()
-        // });
-    })
-
+  
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  
+    socket.on('createMessage', (message, callback) => {
+      console.log('createMessage', message);
+      io.emit('newMessage', generateMessage(message.from, message.text));
+      callback('This is from the server.');
+      // socket.broadcast.emit('newMessage', {
+      //   from: message.from,
+      //   text: message.text,
+      //   createdAt: new Date().getTime()
+      // });
+    });
+  
     socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    })
-})
-
-server.listen(port, () => {
+      console.log('User was disconnected');
+    });
+  });
+  
+  server.listen(port, () => {
     console.log(`Server is up on ${port}`);
-});
+  });
   
 
